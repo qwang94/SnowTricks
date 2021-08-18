6953ae94db0bd57d19645e240906145dfaf5c979
Qichao WANG <qichaowang94@gmail.com>
2021-08-19 00:45:39 +0200

    Update registrationform and starting login form creation

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -22,7 +22,7 @@ class RegistrationFormType extends AbstractType
             ->add('username', null, [
               'label' => 'username',
               'attr' => [
-                'placeholder' => 'username',
+                'placeholder' => 'Username',
               ],
               'row_attr' => [
                 'class' => 'form-floating',
@@ -31,7 +31,7 @@ class RegistrationFormType extends AbstractType
             ->add('firstname', null, [
               'label' => 'firstname',
               'attr' => [
-                'placeholder' => 'firstname',
+                'placeholder' => 'Firstname',
               ],
               'row_attr' => [
                 'class' => 'form-floating',
@@ -40,7 +40,7 @@ class RegistrationFormType extends AbstractType
             ->add('lastname', null, [
               'label' => 'lastname',
               'attr' => [
-                'placeholder' => 'lastname',
+                'placeholder' => 'Lastname',
               ],
               'row_attr' => [
                 'class' => 'form-floating',
